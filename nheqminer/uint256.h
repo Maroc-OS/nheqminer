02412cbefb2c3824f0b972aafeb1983ffd06cab7
Yahya Lmallas <Maroc-OS@users.noreply.github.com>
2017-06-09 03:03:58 +0000

    some old stuff that i did not committed, based on an old upstream with fixes

--- nheqminer/uint256.h
+++ nheqminer/uint256.h
@@ -1,11 +1,12 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin Core developers
+// Copyright (c) 2009-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_UINT256_H
 #define BITCOIN_UINT256_H
 
+#include "crypto/common.h"
 #include <assert.h>
 #include <cstring>
 #include <stdexcept>
@@ -17,7 +18,7 @@
 template <unsigned int BITS> class base_blob {
 protected:
   enum { WIDTH = BITS / 8 };
-  alignas(uint32_t) uint8_t data[WIDTH];
+  uint8_t data[WIDTH];
 
 public:
   base_blob() { memset(data, 0, sizeof(data)); }
@@ -33,14 +34,18 @@ public:
 
   void SetNull() { memset(data, 0, sizeof(data)); }
 
+  inline int Compare(const base_blob &other) const {
+    return memcmp(data, other.data, sizeof(data));
+  }
+
   friend inline bool operator==(const base_blob &a, const base_blob &b) {
-    return memcmp(a.data, b.data, sizeof(a.data)) == 0;
+    return a.Compare(b) == 0;
   }
   friend inline bool operator!=(const base_blob &a, const base_blob &b) {
-    return memcmp(a.data, b.data, sizeof(a.data)) != 0;
+    return a.Compare(b) != 0;
   }
   friend inline bool operator<(const base_blob &a, const base_blob &b) {
-    return memcmp(a.data, b.data, sizeof(a.data)) < 0;
+    return a.Compare(b) < 0;
   }
 
   std::string GetHex() const;
@@ -58,17 +63,19 @@ public:
 
   unsigned int size() const { return sizeof(data); }
 
-  unsigned int GetSerializeSize(int nType, int nVersion) const {
-    return sizeof(data);
+  uint64_t GetUint64(int pos) const {
+    const uint8_t *ptr = data + pos * 8;
+    return ((uint64_t)ptr[0]) | ((uint64_t)ptr[1]) << 8 |
+           ((uint64_t)ptr[2]) << 16 | ((uint64_t)ptr[3]) << 24 |
+           ((uint64_t)ptr[4]) << 32 | ((uint64_t)ptr[5]) << 40 |
+           ((uint64_t)ptr[6]) << 48 | ((uint64_t)ptr[7]) << 56;
   }
 
-  template <typename Stream>
-  void Serialize(Stream &s, int nType, int nVersion) const {
+  template <typename Stream> void Serialize(Stream &s, int nType, int nVersion) const {
     s.write((char *)data, sizeof(data));
   }
 
-  template <typename Stream>
-  void Unserialize(Stream &s, int nType, int nVersion) {
+  template <typename Stream> void Unserialize(Stream &s, int nType, int nVersion) {
     s.read((char *)data, sizeof(data));
   }
 };
@@ -104,19 +111,8 @@ public:
    * when the value can easily be influenced from outside as e.g. a network
    * adversary could
    * provide values to trigger worst-case behavior.
-   * @note The result of this function is not stable between little and big
-   * endian.
    */
-  uint64_t GetCheapHash() const {
-    uint64_t result;
-    memcpy((void *)&result, (void *)data, 8);
-    return result;
-  }
-
-  /** A more secure, salted hash function.
-   * @note This hash is not stable between little and big endian.
-   */
-  uint64_t GetHash(const uint256 &salt) const;
+  uint64_t GetCheapHash() const { return ReadLE64(data); }
 };
 
 /* uint256 from const char *.
